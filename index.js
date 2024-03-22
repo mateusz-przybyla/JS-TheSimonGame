746dd8a30ade1e6028ecd2d248a77e26f0801a9e
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-03-22 19:53:08 +0100

    Show the sequence to the user with animations and sounds

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).animate({ opacity: 0 }, 100);
+  $("#" + randomChosenColour).animate({ opacity: 100 });
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
 
 nextSequence();
